50d1b8d4fdcefccc5598690b30ec705d358c5866
mnoworyta <mnoworyt@gmail.com>
2023-05-12 14:50:57 +0200

    zmiany po code review

--- features/is_off_category_404.feature
+++ features/is_off_category_404.feature
@@ -1,3 +1,3 @@
-Feature: Category status
-  Scenario: Is off category 400?
-     Given Check off category status
+#Feature: Category status
+#  Scenario: Is off category 400?
+#     Given Check off category status
--- features/steps/is_off_category_404.py
+++ features/steps/is_off_category_404.py
@@ -1,22 +1,22 @@
-from settings import SITE
-import logging
-import requests
-from behave import *
-
-
-
+# from settings import SITE
+# import logging
+# import requests
+# from behave import *
+#
+#
+#
 # SITE_TEMP = SITE + "c/wyl-kategoria"
-SITE_TEMP = "https://www.morele.net/qwe/"
-
-
-@given('Check off category status')
-def get_status(context):
-    site_response = requests.get(SITE_TEMP, timeout=3)
-    mark_as_failed = False
-    if site_response.status_code == 404:
-        pass
-    else:
-        logging.error("  Scenario: Is off category 404?           |" + "   SITE STATUS IS: " + str(site_response))
-        assert mark_as_failed is True
-
-
+#
+#
+#
+# @given('Check off category status')
+# def get_status(context):
+#     site_response = requests.get(SITE_TEMP, timeout=3)
+#     mark_as_failed = False
+#     if site_response.status_code == 404:
+#         pass
+#     else:
+#         logging.error("  Scenario: Is off category 404?                 |" + "   SITE STATUS IS: " + str(site_response))
+#         assert mark_as_failed is True
+#
+#
