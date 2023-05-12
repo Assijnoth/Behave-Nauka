6e2b0e92e223f0c5520af9d19e1ad440a06158ba
mnoworyta <mnoworyt@gmail.com>
2023-05-12 11:43:04 +0200

    log correct

--- features/steps/is_off_category_404.py
+++ features/steps/is_off_category_404.py
@@ -4,12 +4,13 @@ from settings import *
 SITE_TEMP = SITE + "c/wyl-kategoria"
 
 
+
 @given('Check if off category is 404')
 def get_status(context):
-    SITE_STATUS = requests.get(SITE_TEMP, timeout=5)
+    SITE_STATUS = requests.get(SITE_TEMP, timeout=3)
     MARK_AS_FAILED = False
     if SITE_STATUS.status_code == 404:
-        logging.error("  Checking if off category is 404                |   CATEGORY SITE IS 404")
+        logging.error("  Scenario: Is category 404                      |   CATEGORY IS 404: " + SITE_TEMP)
         assert MARK_AS_FAILED is True
     else:
         pass
