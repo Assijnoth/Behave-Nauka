ba9c1d1d9971b7af2ecfcd576b724559a4f8ec9c
mnoworyta <mnoworyt@gmail.com>
2023-05-17 10:50:34 +0200

    Poprawki po code review shootera

--- features/steps/pwa_signin.py
+++ features/steps/pwa_signin.py
@@ -1,65 +1,63 @@
-from settings import DRIVERS, SITE
-import logging
-from selenium import webdriver
-from behave import *
-from selenium.webdriver.common.by import By
-
-@given('open browser')
-def openBrowser(context):
-    context.driver = webdriver.Firefox(executable_path=DRIVERS)
-
-@when('open site homepage')
-def openSiteHomepage(context):
-    context.driver.get(SITE)
-
-@then('go to login section')
-def goToLogin(context):
-    try:
-        context.driver.implicitly_wait(3)
-        context.driver.find_element(By.CSS_SELECTOR, ".btn-icons-h").click()
-    except:
-        logging.error("  Scenario: Signin with various logins           |   NOT FOUND LOGIN MODULE")
-        context.driver.close()
-
-@then('enter username {login} and password {password}')
-def enterUserdata(context, login, password):
-    try:
-        context.driver.implicitly_wait(3    )
-        context.driver.find_element_by_xpath("//input[@placeholder='E-mail']").clear()
-        context.driver.find_element_by_xpath("//input[@placeholder='E-mail']").send_keys(login)
-        context.driver.find_element_by_xpath("//input[@placeholder='Hasło']").clear()
-        context.driver.find_element_by_xpath("//input[@placeholder='Hasło']").send_keys(password)
-        context.driver.find_element(By.CSS_SELECTOR, "button.button-basic:nth-child(5)").click()
-    except:
-        logging.error("  Scenario: Signin with various logins           |   NOT FOUND PASSWORD MODULE")
-        context.driver.close()
-
-
-#Check if LOGOUT is available
-
-@then('check login {login} is success')
-def loginSuccespageVerify(context, login):
-    context.driver.implicitly_wait(2)
-    try:
-        status = context.driver.find_element(By.CSS_SELECTOR, "div.link:nth-child(4)").is_displayed()
-        assert status is True
-    except:
-        logging.error("  Scenario: Signin with various logins           |   INVALID LOGIN/PASSWORD FOR USER " + login)
-        context.driver.close()
-
-@then('close browser window')
-def closeBrowserWindow(context):
-    context.driver.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
+# from settings import DRIVERS, SITE
+# import logging
+# from selenium import webdriver
+# from behave import given, when, then
+# from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import NoSuchElementException
+#
+#
+# @given('open browser')
+# def open_browser(context):
+#     context.driver = webdriver.Firefox(executable_path=DRIVERS)
+#
+#
+# @when('open site homepage')
+# def open_site_homepage(context):
+#     context.driver.implicitly_wait(3)
+#     try:
+#         context.driver.get(SITE)
+#     except:
+#         logging.error("  Scenario: Signin with various logins           |" + "   SITE NOT FOUND")
+#         context.driver.close()
+#
+#
+# @then('go to login section')
+# def go_to_login(context):
+#     try:
+#         context.driver.implicitly_wait(3)
+#         context.driver.find_element(By.CSS_SELECTOR, ".btn-icons-h").click()
+#     except NoSuchElementException:
+#         logging.error("  Scenario: Signin with various logins           |   NOT FOUND LOGIN MODULE")
+#         context.driver.close()
+#
+#
+# @then('enter username {login} and password {password}')
+# def enter_userdata(context, login, password):
+#     try:
+#         context.driver.implicitly_wait(3)
+#         context.driver.find_element_by_xpath("//input[@placeholder='E-mail']").clear()
+#         context.driver.find_element_by_xpath("//input[@placeholder='E-mail']").send_keys(login)
+#         context.driver.find_element_by_xpath("//input[@placeholder='Hasło']").clear()
+#         context.driver.find_element_by_xpath("//input[@placeholder='Hasło']").send_keys(password)
+#         context.driver.find_element(By.CSS_SELECTOR, "button.button-basic:nth-child(5)").click()
+#     except NoSuchElementException:
+#         logging.error("  Scenario: Signin with various logins           |   NOT FOUND PASSWORD MODULE")
+#         context.driver.close()
+#
+# # Check if LOGOUT is available
+#
+#
+# @then('check login {login} is success')
+# def login_succespage_verify(context, login):
+#     context.driver.implicitly_wait(2)
+#     try:
+#         status = context.driver.find_element(By.CSS_SELECTOR, "div.link:nth-child(4)").is_displayed()
+#         assert status is True
+#     except NoSuchElementException:
+#         logging.error("  Scenario: Signin with various logins           |   INVALID LOGIN/PASSWORD FOR USER " + login)
+#         context.driver.close()
+#
+#
+# @then('close browser window')
+# def close_browser_window(context):
+#     context.driver.close()
